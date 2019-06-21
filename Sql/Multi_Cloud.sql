e27215f5c9be0f4447b54d4a7d5e59f7f75b8f94
Arkil Thakkar <thakkararkil1996@gmail.com>
2019-06-20 22:30:01 -0700

    constraints to table and created view

--- Sql/Multi_Cloud.sql
+++ Sql/Multi_Cloud.sql
@@ -122,4 +122,35 @@ create table csp_contracts
   primary key(ca_id,csp_id)
 );
 
+create view order_customer as select order_id, order_date, number_of_machines, ca_id, customer_id, cpu_cores, ram, disk_size, order_end_date, order_amount from order_;
 
+create view order_csp as select ord.order_id, ord.order_date, ord.number_of_machines, ord.ca_id, r.csp_id, ord.cpu_cores, ord.ram, ord.disk_size, ord.order_end_date, ord.order_cost
+from order_ ord, receives r where ord.order_id=r.order_id;
+
+create view machine_customer as select mac_id, disk_size, ram, cpu_cores, ip_address, order_id from machine;
+
+create view customer_bill as select bill_id, customer_id, ca_id, month, year, bill_amount, is_paid from bill where csp_id is null;
+
+create view ca_bill as select bill_id, ca_id, csp_id, month, year, bill_amount, is_paid from bill where customer_id is null;
+
+
+
+
+alter table order_ add constraint fk_order_ca_id foreign key (ca_id) references ca(ca_id) ;
+alter table order_ add constraint fk_order_customer_id foreign key (customer_id) references customer(customer_id);
+
+
+alter table bill add constraint fk_bill_csp_id foreign key (csp_id) references csp(csp_id);
+alter table bill add constraint fk_bill_ca_id foreign key (ca_id) references ca(ca_id);
+alter table bill add constraint fk_bill_cust_id foreign key (customer_id ) references customer(customer_id);
+alter table machine add constraint fk_machine_csp_id foreign key (csp_id) references csp(csp_id);
+alter table machine add constraint fk_machine_order_id foreign key (order_id) references order_(order_id);
+alter table receives add constraint fk_receives_csp_id foreign key (csp_id) references csp(csp_id);
+alter table receives add constraint fk_receives_order_id foreign key (order_id) references order_(order_id);
+alter table onboards add constraint fk_onboards_ca_id foreign key (ca_id) references ca(ca_id);
+alter table onboards add constraint fk_onboards_customer_id foreign key (customer_id) references customer(customer_id);
+alter table customer add constraint fk_customer_offer_id foreign key (customer_offer_id)  references offer(offer_id) on delete set null;
+alter table offer add constraint fk_offer_ca_id foreign key (ca_id) references ca(ca_id);
+
+-- alter table csp_contracts add constraint fk_csp_contracts_csp_id foreign key (csp_id) references csp(csp_id);
+-- alter table csp_contracts add constraint fk_csp_contracts_ca_id foreign key (ca_id) references ca(ca_id);
