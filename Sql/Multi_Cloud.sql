d068cdc6881ee23ddc68788bb13a011148a55d70
Arkil Thakkar <thakkararkil1996@gmail.com>
2019-06-20 22:16:19 -0700

    Create anothertables

--- Sql/Multi_Cloud.sql
+++ Sql/Multi_Cloud.sql
@@ -40,3 +40,86 @@ create table ca
   ca_password varchar(255) not null,
   primary key(ca_id)
 );
+
+
+create table customer
+(
+  customer_id int not null auto_increment,
+  customer_email_id varchar(255) not null,
+  customer_name char(255) not null,
+  customer_password varchar(255) not null,
+  customer_join_date date not null,
+  customer_bank_account bigint(16) not null,
+  customer_offer_id int,
+  customer_isDelete boolean default false,
+  primary key(customer_id)
+);
+
+
+create table bill
+(
+  bill_id int not null auto_increment,
+  bill_amount int(12) not null,
+  csp_id int,
+  ca_id int not null,
+  customer_id int,
+  month int not null,
+  year int not null,
+  is_paid bool default False,
+  primary key(bill_id)
+);
+
+create table offer
+(
+  offer_id int not null auto_increment,
+  offer_name varchar(255) not null,
+  discount int not null,
+  ca_id int not null,
+  valid_till date,
+  is_used bool default False,
+  primary key(offer_id)
+);
+
+
+
+create table machine
+(
+  mac_id int not null auto_increment,
+  csp_id int not null,
+  # gpu varchar(20) not null,
+  disk_size varchar(20) not null,
+  ram int(4) not null,
+  cpu_cores int(4) not null,
+  # os char(20) not null,
+  ip_address varchar(16) not null,
+  price int not null,
+  order_id int,
+  primary key(mac_id, csp_id)
+);
+
+
+create table receives
+(
+  csp_id int not null,
+  order_id int not null,
+  quantity int not null,
+  csp_cost int not null,
+  primary key (csp_id,order_id)
+);
+
+create table onboards
+(
+  ca_id int not null,
+  customer_id int not null,
+  primary key (ca_id, customer_id)
+);
+
+
+create table csp_contracts
+(
+  ca_id int not null,
+  csp_id int not null,
+  primary key(ca_id,csp_id)
+);
+
+
